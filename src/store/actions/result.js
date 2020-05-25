af9d11aaedf101288b0350f5899db96b4dc0666a
rohan <rohan@brahmin-solutions.com>
2020-05-25 19:06:37 +0530

    Where to put data transforming logic ?

--- src/store/actions/result.js
+++ src/store/actions/result.js
@@ -1,6 +1,8 @@
 import * as actionTypes from "../actions/actionTypes";
 
 export const saveResult = (res) => {
+  //   Change data before saving to store 1
+  //   const updatedResult = res * 2;
   return {
     type: actionTypes.STORE_RESULT,
     result: res,
--- src/store/reducer/results.js
+++ src/store/reducer/results.js
@@ -7,6 +7,7 @@ const initialState = {
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.STORE_RESULT:
+      //Change data before saving to store 1
       return {
         ...state,
         results: state.results.concat({ id: new Date(), value: action.result }),
