b8eb74eced960ad28e682edd73e0204579725809
rohan <rohan@brahmin-solutions.com>
2020-05-25 18:59:24 +0530

    Restructuring Actions

---
+++ src/store/actions/result.js
@@ -0,0 +1,23 @@
+import * as actionTypes from "../actions/actionTypes";
+
+export const saveResult = (res) => {
+  return {
+    type: actionTypes.STORE_RESULT,
+    result: res,
+  };
+};
+
+export const storeResult = (res) => {
+  return (dispatch) => {
+    setTimeout(() => {
+      dispatch(saveResult(res));
+    }, 2000);
+  };
+};
+
+export const deleteResult = (value) => {
+  return {
+    type: actionTypes.DELETE_RESULT,
+    resultElId: value,
+  };
+};
